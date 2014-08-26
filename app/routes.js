97c5d70da408575a86a4b5b0cd7d562f45963677
Hyiero <hyiero89@gmail.com>
2014-08-26 10:33:52 -0400

    New mean stack should be ready to deploy on the qa site

--- app/routes.js
+++ app/routes.js
@@ -51,11 +51,11 @@ module.exports = function(app, passport) {
 //Profile Section ===================
 //We will want this protected so you have to be logged in to visit
 //We will use route middleware to verify this(the isLoggedIn function)
-//    app.get('/profile', isLoggedIn, function (req, res) {
-//        res.render('profile.ejs', {
-//            user: req.user //get the user out of session and pass to template
-//        });
-//    });
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user //get the user out of session and pass to template
+        });
+    });
 
 //Change the Scene ===================
     app.post('/changeScene', function (req, res) {
--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://127.0.0.1:27017/test'
+	url : 'mongodb://3QA:5d4b7d67325f227761473a2866@novus.modulusmongo.net:27017/asix2Odo'
 }
