4220bc9e9abd86032f14941bd6add6bb1b683cbc
Kevin Curry <kmcurry@gmail.com>
2014-09-16 19:18:16 -0400

    add fb pixel to signup

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -27,3 +27,18 @@ function postPictureToFacebook() {
             $("#example").dialog("open");
         }, 1000);
 }
+
+<!-- Facebook Conversion Code for 3Scape Registrations -->
+(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6017394824149', {'value':'0.00','currency':'USD'}]);
