2ec67efe577504419a7ea465dae9f178c937d6f5
Kevin Curry <kmcurry@gmail.com>
2014-10-26 22:15:59 -0400

    uncomment id

--- public/js/editor.js
+++ public/js/editor.js
@@ -349,7 +349,7 @@ function loadModel(url)
 
     var objectPanel = document.getElementById("object-panel");
     var row = document.createElement('div');
-    //row.setAttribute("id", "row" + name);
+    row.setAttribute("id", "row" + name);
     //row.setAttribute("class", "row" + name);
     var nameColumn = document.createElement('div');
     // var colorColumn = document.createElement('div');
