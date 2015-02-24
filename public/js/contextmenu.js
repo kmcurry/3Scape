37de5cb830a4260581beeefdace6eb6e8d847725
soffos <soffos@gateway.cs.odu.edu>
2015-02-23 23:57:28 -0500

    Right click now selects

--- public/js/contextmenu.js
+++ public/js/contextmenu.js
@@ -1,14 +1,43 @@
 document.addEventListener('contextmenu', function(e) {
   if (e.button === 2) {
+    if (g_selectedModel)
+    {
+        g_selectedModel.getAttribute("highlight").setValueDirect(false);
+        g_selectedModel = null;
+    }
+    if (bridgeworks.selector.selections.models.length > 0)
+    {
+        g_selectedModel = bridgeworks.selector.selections.models[0];
+    }
+
+    if (g_selectedModel) {
+
+        g_selectedModelName = g_selectedModel.name.getValueDirect().join("");
+        console.log(g_selectedModelName);
+
+        if (g_selectedModelName != 'Grid') {
+
+            if (g_selectedModel.moveable.getValueDirect()) {
+              g_selectedModel.getAttribute("highlight").setValueDirect(true);
+            }
+
+        } else { g_selectedModel = null; $("#model-menu").prop('disabled',true);} //turn off context menu on deselect
+    }
+    else {
+
+        console.log("NO MODEL SELECTED");
+        g_selectedModel = null;
+
+    }
     if (g_selectedModel) {
       positionMenu(e, modelMenu);
       $("#model-menu").toggleClass("active");
     }
+
     e.preventDefault();
     return false;
   }
 }, false);
-
 
 function positionMenu(e, menu) {
   var xPosition = e.clientX - (menu.clientWidth / 2);
