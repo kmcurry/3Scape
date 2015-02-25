b1aba84004d406920476563765ef6e676220db5a
Kevin Curry <kmcurry@gmail.com>
2015-02-24 23:49:05 -0500

    add rotation controls

--- public/js/snappy.js
+++ public/js/snappy.js
@@ -117,10 +117,17 @@ function roam(name) {
     bridgeworks.updateScene(cmd);
 }
 
+function rotateDown() {
+  if (g_selectedModel) {
+    var sRot = g_selectedModel.rotation.getValueDirect();
+    g_selectedModel.rotation.setValueDirect(sRot.x-10, sRot.y, sRot.z);
+  }
+}
+
 function rotateLeft() {
   if (g_selectedModel) {
     var sRot = g_selectedModel.rotation.getValueDirect();
-    g_selectedModel.rotation.setValueDirect(sRot.x, sRot.y-45, sRot.z);
+    g_selectedModel.rotation.setValueDirect(sRot.x, sRot.y-10, sRot.z);
   }
 }
 
@@ -128,7 +135,14 @@ function rotateLeft() {
 function rotateRight() {
   if (g_selectedModel) {
     var sRot = g_selectedModel.rotation.getValueDirect();
-    g_selectedModel.rotation.setValueDirect(sRot.x, sRot.y+45, sRot.z);
+    g_selectedModel.rotation.setValueDirect(sRot.x, sRot.y+10, sRot.z);
+  }
+}
+
+function rotateUp() {
+  if (g_selectedModel) {
+    var sRot = g_selectedModel.rotation.getValueDirect();
+    g_selectedModel.rotation.setValueDirect(sRot.x+10, sRot.y, sRot.z);
   }
 }
 
