8461763272b40fce00601fddcebc2f96f1e05680
Katherine Landreth <katlandreth@gmail.com>
2015-04-06 13:05:14 -0400

    makes the color wheel work on parts - does not get the current color of the part

--- public/js/colorwheel.js
+++ public/js/colorwheel.js
@@ -361,11 +361,12 @@ Raphael.colorwheel = function(target, color_wheel_size, no_segments){
     
     cw.onchange(function(color){
         var rawColor = cw.color(),
-            rColor = Math.round(rawColor.r),
-            gColor = Math.round(rawColor.g),
-            bColor = Math.round(rawColor.b),
+            rColor = Math.round(rawColor.r)/255,
+            gColor = Math.round(rawColor.g)/255,
+            bColor = Math.round(rawColor.b)/255,
             thisColor = rColor + ", " + gColor + ", " + bColor; 
         console.log("rgb(" + thisColor + ")");
+        g_selectedModel.color.setValueDirect(rColor, gColor, bColor, 1);
     });
 
 }); 
