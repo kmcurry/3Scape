b1a5a65af47e3939e4424c411ddf8a389fb840cc
Kat Landreth <katlandreth@gmail.com>
2015-04-13 14:56:39 -0400

    makes the hamburger menu icon responsive

--- public/js/tablet-resize.js
+++ public/js/tablet-resize.js
@@ -1,14 +1,15 @@
 function setHeights(){
         var ew = $('.innerdial').css("width");
-        var fontSize = parseInt(ew)/2; 
         var partWidth = $('li.part').css('width');
+        var fontSize = parseInt(partWidth)*.75; 
         console.log(partWidth);
         var buttonWidth = $('button.parts-btn').css('width');
         $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
         $('dial span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
         $('li.part').css({'line-height':partWidth});
-        $('.nav-logo').css({'height': partWidth});
+        $('.nav-logo, .nav-menu').css({'height': partWidth});
         $('button.parts-btn').css({'height': buttonWidth}); 
+        $('.nav-menu').css({'font-size': fontSize});
     }
 
 $(document).ready(function(){
