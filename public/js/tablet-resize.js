1f68fd3c63199b4eef97ebac03a40dd6aac61094
Kevin Curry <kmcurry@gmail.com>
2015-04-15 16:05:25 -0400

    partial check in for toggling partsbar and actionsbar

--- public/js/tablet-resize.js
+++ public/js/tablet-resize.js
@@ -4,15 +4,16 @@ function setHeights(){
         var fontSize = parseInt(ew)/2;
 
         var partWidth = $('li.part').css('width');
-        var fontSize = parseInt(partWidth)*.75;
-        console.log(partWidth);
+        //var fontSize = parseInt(partWidth)*.75;
+        var partWidthString = "'" + partWidth + "'";
+        console.log(partWidthString);
         var buttonWidth = $('button.parts-btn').css('width');
         $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
         $('dial span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
-
-        $('li.part').css({'height':partWidth});
-        $('.logo, .nav-menu').css({'height': partWidth});
+        $('li.part').css({'height': partWidthString});
+        $('.nav-logo, .nav-menu').css({'height': partWidthString});
         $('button.parts-btn').css({'height': buttonWidth});
+
 
     }
 
