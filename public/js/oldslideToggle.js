203ef4c4ca52567c8007d8d20d89975a4211bcfe
Kat Landreth <katlandreth@gmail.com>
2015-04-16 12:08:38 -0400

    makes bars slide up and down on hover without firing multiple times and smooths animation

---
+++ public/js/oldslideToggle.js
@@ -0,0 +1,7 @@
+<div class="navbar-fixed-top" onmouseover="$('.parts-bar').slideDown(500)" onmouseout="$('.parts-bar').slideUp(500)">
+    	<%- include('viewcontrols/partsbar'); %>
+		</div>
+
+		<div class="navbar-fixed-bottom" onmouseover="$('.actions-bar').slideDown(500)" onmouseout="$('.actions-bar').slideUp()">
+			<%- include('viewcontrols/actionsbar') %>
+		</div>
