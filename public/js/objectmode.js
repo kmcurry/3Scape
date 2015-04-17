2ee3fa7694645b2b24545d9b9665b1e39155a321
Kevin Curry <kmcurry@gmail.com>
2015-04-17 15:35:27 -0400

    fix grower and add type names back

--- public/js/objectmode.js
+++ public/js/objectmode.js
@@ -131,10 +131,10 @@ function addRemoveFader() {
   if (g_selectedModel && g_selectedModel.moveable.getValueDirect()) {
     var name = g_selectedModel.name.getValueDirect().join("");
     var cmd = "";
-    if (bridgeworks.get(name + '_AutoInterpolator')) {
-      cmd = "<Update><Remove target='" + name + "_AutoInterpolator'/><Set target='" + name + "' opacity='1'/></Update>";
+    if (bridgeworks.get('Fader_' + name + '_AutoInterpolator')) {
+      cmd = "<Update><Remove target='Fader_" + name + "_AutoInterpolator'/><Set target='" + name + "' opacity='1'/></Update>";
     } else {
-      cmd = "<AutoInterpolate name='" + name + "' postBehavior='3' target='" + name + "' opacity='0'/>";
+      cmd = "<AutoInterpolate name='Fader_" + name + "' postBehavior='3' target='" + name + "' opacity='0'/>";
     }
     console.log(cmd);
     bridgeworks.updateScene(cmd);
@@ -144,7 +144,7 @@ function addRemoveFader() {
 function setFaderDuration(duration) {
   if (g_selectedModel) {
     var name = g_selectedModel.name.getValueDirect().join("");
-    var cmd = "<Set target='" + name + "_AutoInterpolator' duration='" + duration + "'/>";
+    var cmd = "<Set target='Fader_" + name + "_AutoInterpolator' duration='" + duration + "'/>";
     console.log(cmd);
     bridgeworks.updateScene(cmd);
   }
@@ -154,8 +154,8 @@ function addRemoveGrower() {
   if (g_selectedModel && g_selectedModel.moveable.getValueDirect()) {
     var name = g_selectedModel.name.getValueDirect().join("");
     var cmd = "";
-    if (bridgeworks.get('Grower_' + name)) {
-      cmd = "<Update><Remove target='Grower_" + name + "'/><Set target='" + name + "' opacity='1'/></Update>";
+    if (bridgeworks.get('Grower_' + name + '_AutoInterpolator')) {
+      cmd = "<Update><Remove target='Grower_" + name + "_AutoInterpolator'/><Set target='" + name + "' opacity='1'/></Update>";
     } else {
       cmd = "<AutoInterpolate name='Grower_" + name + "' postBehavior='3' target='" + name + "'><scale x='3' y='3' z='3'/></AutoInterpolate>";
     }
