807d4608344d50acadcc25f0e36ce014b725f86b
Kat Landreth <katlandreth@gmail.com>
2015-04-22 08:24:52 -0400

    removes extra two pixels from parts section of top bar

--- public/js/tablet-resize.js
+++ public/js/tablet-resize.js
@@ -2,12 +2,12 @@ function setHeights(){
         var ew = $('.innerdial').css('width');
         var fontSize = parseInt(ew)/2;
         console.log('fontSize = ' + fontSize + ' ew = ' + ew);
-        var barHeight = $('.parts-bar').css('height');
+//        var barHeight = $('.parts-bar').css('height');
         var buttonWidth = $('button.parts-btn').css('width');
         $('.innerdial, .meter-outer, #meter-inner').css({'height':ew}); //makes height = fluid width
         $('dial span.fa').css({'line-height':ew, 'font-size': fontSize}); //makes icon in dial responsive to dial size change
-        $('li.part').css({'height': barHeight});
-        $('.logo, .nav-menu').css({'height': barHeight});
+        $('li.part').css({'height': buttonWidth});
+        $('.logo, .nav-menu').css({'height': buttonWidth});
         $('button.parts-btn').css({'height': buttonWidth});
 
 
