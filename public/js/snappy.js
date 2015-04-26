3b9dd126b46a4526e6ffb772c5631b29fe6002dc
Kevin Curry <kmcurry@gmail.com>
2015-04-25 21:40:33 -0400

    comment out post on new

--- public/js/snappy.js
+++ public/js/snappy.js
@@ -73,6 +73,7 @@ function new3Scape() {
     bridgeworks.updateScene('grid-50.xml');
 
     // QTP: post to server right away or wait until an autosave event?
+    /*
     $.ajax({
       url: 'new',
       type: 'POST',
@@ -81,6 +82,7 @@ function new3Scape() {
       success: updateLocalStorage
     });
     window.location.pathname = "/";
+    */
 }
 
 function updateLocalStorage(scapeId) {
