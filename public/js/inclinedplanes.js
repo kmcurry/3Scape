372c64a3f99a61f4649c4a4ff0d1fe029648eb0a
Kat Landreth <katlandreth@gmail.com>
2015-05-04 16:48:06 -0400

    changes colors of wedges and spheres, adjust lighting to better see dimensions, makes gravity button work on refresh

--- public/js/inclinedplanes.js
+++ public/js/inclinedplanes.js
@@ -5,9 +5,13 @@ $(document).ready(function(){
     document.body.appendChild(button);
     $("button[value='gravity']").addClass("gravity");
     $("button[value='gravity']").text("Gravity Button");
-    $("button.gravity").click(function(data){
-      var model = bridgeworks.get("Sphere1");
-      model.physicsEnabled.setValueDirect(true);
-      console.log(model.physicsEnabled.getValueDirect());
+    $("button.gravity").click(function(){
+      var sphere1 = bridgeworks.get("Sphere1"),
+          sphere2 = bridgeworks.get("Sphere2"),
+          sphere3= bridgeworks.get("Sphere3");
+      sphere1.physicsEnabled.setValueDirect(true);
+      sphere2.physicsEnabled.setValueDirect(true);
+      sphere3.physicsEnabled.setValueDirect(true);
+      console.log("end of button function");
     });
 });
