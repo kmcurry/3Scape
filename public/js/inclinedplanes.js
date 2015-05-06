1256c211ab11e3d91ca7613a112bc739ffff5967
Kat Landreth <katlandreth@gmail.com>
2015-05-06 12:16:51 -0400

    makes reset button work and roughs in the labels

--- public/js/inclinedplanes.js
+++ public/js/inclinedplanes.js
@@ -1,31 +1,47 @@
 $(document).ready(function(){
-  var button = document.createElement("button");
-  button.type = "submit";
-  button.value = "gravity";
-  document.body.appendChild(button);
-  $("button[value='gravity']").addClass("gravity");
-  $("button[value='gravity']").text("Gravity Button");
+  
+//  var sphere1 = bridgeworks.get("Sphere1"),
+//      sphere2 = bridgeworks.get("Sphere2"),
+//      sphere3 = bridgeworks.get("Sphere3");
+  
+  function addButtons(){
+    var gravButton = document.createElement("button"),
+        resetButton = document.createElement("button");
+    gravButton.type = "submit";
+    gravButton.value = "gravity";
+    resetButton.type = "submit";
+    resetButton.value = "reset";
+    document.body.appendChild(gravButton);
+    document.body.appendChild(resetButton);
+    $("button[value='gravity']").addClass("gravity wedges-btn");
+    $("button.gravity").text("Gravity Button");
+    $("button[value='reset']").addClass("reset wedges-btn");
+    $("button.reset").text("Reset Button");
+};
+  addButtons();
+  
   $("button.gravity").click(function(){
-      var sphere1 = bridgeworks.get("Sphere1"),
-      sphere2 = bridgeworks.get("Sphere2"),
-      sphere3= bridgeworks.get("Sphere3");
+    sphere1 = bridgeworks.get("Sphere1"),
+    sphere2 = bridgeworks.get("Sphere2"),
+    sphere3 = bridgeworks.get("Sphere3");
     sphere1.physicsEnabled.setValueDirect(true);
     sphere2.physicsEnabled.setValueDirect(true);
     sphere3.physicsEnabled.setValueDirect(true);
     console.log("end of button function");
   });
+  
+  $("button.reset").click(function(){
+    sphere1 = bridgeworks.get("Sphere1"),
+    sphere2 = bridgeworks.get("Sphere2"),
+    sphere3 = bridgeworks.get("Sphere3");
+    sphere1.physicsEnabled.setValueDirect(false);
+    sphere2.physicsEnabled.setValueDirect(false);
+    sphere3.physicsEnabled.setValueDirect(false);
+    sphere1.position.setValueDirect(-10,5,103);
+    sphere2.position.setValueDirect(0,5,102);
+    sphere3.position.setValueDirect(10,5,102);
+    console.log("end of button function");
+  });
 });
 
 
-//  var resetButton = document.createElement("button");
-//    resetButton.type = "submit";
-//    resetButton.value = "reset";
-//    document.body.appendChild(resetButton);
-//    $("button[value='reset']").addClass("gravity reset");
-//    $("button[value='reset']").text("Reset Spheres");
-//    $("button.reset").click(function(){
-//      sphere1.position.setValueDirect();
-//      sphere2.position.setValueDirect();
-//      sphere3.position.setValueDirect();
-//      console.log("end of button function");
-//    });
