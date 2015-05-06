ace64ae76fb7e399d1fff1edf54718f394bb36cb
Kat Landreth <katlandreth@gmail.com>
2015-05-06 14:20:35 -0400

    3scape labels prevent gravity button from working so using a javascript inserted Div for now instead

--- public/js/inclinedplanes.js
+++ public/js/inclinedplanes.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   
-//  var sphere1 = bridgeworks.get("Sphere1"),
-//      sphere2 = bridgeworks.get("Sphere2"),
-//      sphere3 = bridgeworks.get("Sphere3");
+  var sphere1 = "",
+      sphere2 = "",
+      sphere3 = "";
   
   function addButtons(){
     var gravButton = document.createElement("button"),
@@ -14,20 +14,29 @@ $(document).ready(function(){
     document.body.appendChild(gravButton);
     document.body.appendChild(resetButton);
     $("button[value='gravity']").addClass("gravity wedges-btn");
-    $("button.gravity").text("Gravity Button");
+    $("button.gravity").text("Gravity On");
     $("button[value='reset']").addClass("reset wedges-btn");
-    $("button.reset").text("Reset Button");
-};
+    $("button.reset").text("Reset Spheres");
+  };
   addButtons();
   
+  function addLabel(){
+    var labelDiv = document.createElement("div");
+    labelDiv.setAttribute('class', 'labelDiv');
+    document.body.appendChild(labelDiv);
+    $("div.labelDiv").html("<h3>Inclined Planes Experiment</h3><p>3 spheres of the same size are hovering above 3 inclined planes.</p><p>The first inclined plane is shallow, the second is a little steeper, and the third is the steepest.</p><p>When the spheres drop, <b>which one will roll the farthest</b>?</p><p>Push the Gravity On button to find out!</p>");
+  };
+  
+  addLabel();
+  
   $("button.gravity").click(function(){
+    $(".labelDiv").addClass("hideLabel");
     sphere1 = bridgeworks.get("Sphere1"),
     sphere2 = bridgeworks.get("Sphere2"),
     sphere3 = bridgeworks.get("Sphere3");
     sphere1.physicsEnabled.setValueDirect(true);
     sphere2.physicsEnabled.setValueDirect(true);
     sphere3.physicsEnabled.setValueDirect(true);
-    console.log("end of button function");
   });
   
   $("button.reset").click(function(){
@@ -37,10 +46,9 @@ $(document).ready(function(){
     sphere1.physicsEnabled.setValueDirect(false);
     sphere2.physicsEnabled.setValueDirect(false);
     sphere3.physicsEnabled.setValueDirect(false);
-    sphere1.position.setValueDirect(-10,5,103);
-    sphere2.position.setValueDirect(0,5,102);
-    sphere3.position.setValueDirect(10,5,102);
-    console.log("end of button function");
+    sphere1.position.setValueDirect(-10,7,102);
+    sphere2.position.setValueDirect(0,7,102);
+    sphere3.position.setValueDirect(10,7,102);
   });
 });
 
