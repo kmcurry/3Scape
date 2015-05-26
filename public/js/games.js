3d045a8560621d7dcc1220b783c4cffe9c96ddaa
Kat Landreth <katlandreth@gmail.com>
2015-05-26 13:57:39 -0400

    makes a jewel and a collider

--- public/js/games.js
+++ public/js/games.js
@@ -1,51 +1,48 @@
 $(window).load(function(){
-  var blueCubesCount = 150,
-      blueCubesSize = 5,
-      blueCubesStart = {x:"0", y:"10", z:"0"},
-      blueCubesEnd = {x:"50", y:"10", z:"50"},
-      cubePosition = {x:"", y:"", z:""},
-      positionArray = [],
-      c = 0,
-      color = {r:"0.025", g:"0.45", b:"0.50", a:"0.5"},  
-      newStart="";
-      
-  function makeCube(num, size, color, position){
+  function makeJewel(){
     var cmd =  "<Model url='objects/cube.lwo' moveable='true' opacity='1' selectable='true' physicsEnabled='true' detectCollision='true'>";
-        cmd += "<color r='" + color.r + "' g='" + color.g + "' b='" + color.b + "' a='" + color.a + "'/>";
-        cmd += "<scale x='" + size + "' y='" + size + "' z='" + size + "' />"; 
-        cmd += "<position x='" + cubePosition.x + "' y='" + cubePosition.y + "' z='" + cubePosition.z + "' />";  
+        cmd += "<color r='0.45' g='0.025' b='0.34' a='1'/>";
+        cmd += "<scale x='5' y='5' z='5' />"; 
+        cmd += "<position x='0' y='10' z='0' />";  
         cmd += "<physicalProperties>";
-        cmd += "<mass>0</mass>";
+        cmd += "<mass>1</mass>";
         cmd += "</physicalProperties>";
         cmd += "</Model>";
     bridgeworks.updateScene(cmd);
   }
   
-  function cubeParams(count, size, start, end){
-   
-    for (i = 0; i < count; i++) { 
-       console.log("start of loop= "+ start.x);
-      if (cubePosition.x < end.x){
-        cubePosition.x = start.x;
-        cubePosition.y= start.y;
-        cubePosition.z = start.z;
-        start.x = parseInt(start.x) + size*2;
-        console.log(cubePosition.x);
-      }
-//      if(cubePosition.x > end.x){
-//        cubePosition.x = start.x;
-//        cubePosition.y= start.y;
-//        cubePosition.z = start.z - size*2;
-//        start.x = parseInt(start.x) - (size*2);
-//      }
-      
-      console.log("end of loop= "+ start.x);
-      positionArray.push(cubePosition);
-      makeCube(blueCubesCount, blueCubesSize, color, cubePosition);
-    }
-  };
-
-  cubeParams(blueCubesCount, blueCubesSize, blueCubesStart, blueCubesEnd);
-  console.log(positionArray);
+  function makeCollider(){
+    console.log("start collider");
+    var cmd =  "<Model name='collider' url='objects/pyramid.lwo' moveable='true' opacity='1' selectable='true' physicsEnabled='true' detectCollision='true'>";
+        cmd += "<color r='0.00' g='0.00' b='0.00' a='1'/>";
+        cmd += "<scale x='5' y='5' z='5' />"; 
+        cmd += "<position x='-50' y='10' z='0' />";  
+        cmd += "<physicalProperties>";
+        cmd += "<mass>1</mass>";
+        cmd += "</physicalProperties>";
+        cmd += "</Model>";
+    bridgeworks.updateScene(cmd);
+    console.log("end collider");
+  }
+  
+  
+  
+  function collide(){
+    console.log("start collide");
+      cmd = "<Update><AnimalMover name='Roaming_collider' target='collider' linearSpeed='1' angularSpeed='10'/>";
+      cmd += "<Set target='collider' detectCollision='" + true + "' detectObstruction='true'>";
+      cmd += "</Set>"
+      cmd += "</Update>";
+    bridgeworks.updateScene(cmd);
+    console.log("end collide");
+  }
+  
+  makeJewel();
+  
+  setTimeout(function(){
+    makeCollider();
+    collide();
+  
+  }, 1000);
   
 });
