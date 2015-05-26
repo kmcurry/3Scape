0305dbedbc1c5ba7b1ccd9794041f1612d56f50e
Kat Landreth <katlandreth@gmail.com>
2015-05-26 11:14:46 -0400

    working out javascript to auto fill an area with cubes

--- public/js/games.js
+++ public/js/games.js
@@ -1,48 +1,43 @@
 $(window).load(function(){
-  var blueCubesCount = 50,
+  var blueCubesCount = 100,
       blueCubesSize = 1,
-      blueCubesStart = 0,
-      blueCubesEnd = 50,
-      positionX = "",
-      positionY = "",
-      positionZ = "";
-      var newStart="";
+      blueCubesStart = {x:"0", y:"10", z:"0"},
+      blueCubesEnd = {x:"50", y:"10", z:"50"},
+      cubePosition = {x:"", y:"", z:""},
+      positionArray = [],
+      c = 0,
+      color = {r:"0.55", g:"0.025", b:"0.50", a:"1"},  
+      newStart="";
       
-  function makeCube(num, size, cubeR, cubeG, cubeB, cubeA, position){
-    var cmd =  "<Model url='objects/cube.lwo' moveable='true' opacity='1' selectable='true' physicsEnabled='true' detectCollision='true'>";
-        cmd += "<color r='" + cubeR + "' g='" + cubeG + "' b='" + cubeB + "' a='" + cubeA + "'/>";
+  function makeCube(num, size, color, position){
+    var cmd =  "<Model name ='cube'" + c + "url='objects/cube.lwo' moveable='true' opacity='1' selectable='true' physicsEnabled='true' detectCollision='true'>";
+        cmd += "<color r='" + color.r + "' g='" + color.g + "' b='" + color.b + "' a='" + color.a + "'/>";
         cmd += "<scale x='" + size + "' y='" + size + "' z='" + size + "' />"; 
-        cmd += "<position x='" + positionX + "' y='" + positionY + "' z='" + positionZ + "' />";  
+        cmd += "<position x='" + cubePosition.x + "' y='" + cubePosition.y + "' z='" + cubePosition.z + "' />";  
         cmd += "<physicalProperties>";
         cmd += "<mass>0</mass>";
         cmd += "</physicalProperties>";
         cmd += "</Model>";
+        c = c++;
     bridgeworks.updateScene(cmd);
   }
   
-  function cubePosition(count, size, start, end){
-    
+  function cubeParams(count, size, start, end){
+   
     for (i = 0; i < count; i++) { 
-      
-
-      if (positionX < end){
-          positionX = newStart;
-          positionY = size + 10;
-          positionZ = start;
-          newStart = start + size;
-        console.log(cubePosition[i]);
+       console.log("start of loop= "+ start.x);
+      if (cubePosition.x < end.x){
+        cubePosition.x = start.x;
+        cubePosition.y= start.y;
+        cubePosition.z = start.z;
       }
-      else{
-          positionX = newStart;
-          positionY = size + 10;
-          positionZ = size;
-         newStart = start+size;
-      }
+      start.x = parseInt(start.x) + size;
+      console.log("end of loop= "+ start.x);
+      positionArray.push(cubePosition);
     }
   };
-  
-  for(i=0; i< blueCubesCount; i++){
-  makeCube(50, 5, 0.2, 0.5, 0.2, 1, cubePosition(blueCubesCount, blueCubesSize, blueCubesStart, blueCubesEnd));
-  }
-  
+
+  cubeParams(blueCubesCount, blueCubesSize, blueCubesStart, blueCubesEnd);
+  console.log(positionArray);
+  makeCube(blueCubesCount, blueCubesSize, color, positionArray[i]);
 });
